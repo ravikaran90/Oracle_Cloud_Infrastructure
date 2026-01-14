75574baf2b0f69b364546e9b1b81a4acf41c8cbe
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-01-14 00:01:55 -0800

    main.tf for Netskope

---
+++ Netskope/main.tf
@@ -0,0 +1,35 @@
+resource "oci_core_subnet" "networking_subnet" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = var.vcn_ocid
+  cidr_block     = "10.1.1.0/28"
+  display_name   = "Networking Subnet"
+  prohibit_internet_ingress = true
+}
+
+resource "oci_core_instance" "netskope" {
+  availability_domain = "wbka:US-ASHBURN-AD-1"
+  compartment_id      = var.compartment_ocid
+  display_name        = "Netskope Instance"
+  shape               = var.instance_shape
+
+  create_vnic_details {
+    display_name           = "main-vnic"
+    subnet_id              = var.subnet_ocid
+    private_ip             = var.priv_ip
+    assign_public_ip      = false
+    skip_source_dest_check = false
+  }
+  shape_config {
+    memory_in_gbs = 8
+    ocpus         = 2
+  }
+  source_details {
+    source_type = "image"
+    source_id   = var.image_id
+    boot_volume_size_in_gbs = 50
+  }
+  metadata = {
+    ssh_authorized_keys = file("ssh-key-2021-05-12.key.pub")
+  }
+  preserve_boot_volume = false
+}
