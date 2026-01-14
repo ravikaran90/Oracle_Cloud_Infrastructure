2b546328a1364c9be5377703b9f6690f77c5964a
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-01-13 23:43:47 -0800

    Variables for Monitoring Instanceonitoring instance configuration

---
+++ monitoring_instance/variables.tf
@@ -0,0 +1,20 @@
+variable "user_ocid"{
+}
+variable "region"{
+}
+variable "tenancy_ocid"{
+}
+variable "compartment_ocid"{
+}
+variable "vcn_ocid"{
+}
+variable "subnet_ocid"{
+}
+variable "fingerprint"{
+}
+variable "private_key_path"{
+}
+variable "instance_shape"{
+}
+variable "image_id"{
+}
